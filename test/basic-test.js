dd518fdfde782cc961aec43ddd8cfbbc6dfa2990
zjonsson <ziggy.jonsson.nyc@gmail.com>
2023-07-23 20:59:25 -0400

    Test bad zip file

--- test/basic-test.js
+++ test/basic-test.js
@@ -4,10 +4,17 @@ const PdfStream = require('../index');
 const path = require('path');
 const t = require('tap');
 
-t.test('merges two pdf files', async () => {
+t.test('merges two pdf files', async t => {
   const pdfStream = new PdfStream();
   const file1 = createReadStream(path.resolve(__dirname, './test.pdf'));
   await pdfStream.append(file1,{ name: 'file1' });
+  const badFile = createReadStream(path.resolve(__dirname, '../README.md'));
+  await pdfStream.append(badFile, { name: 'badfile'})
+    .then(
+      () => { throw new Error('Non-pdf file should error');},
+      (e) => { t.equal(e.message, 'File badfile is not a zip file'); }
+    );
+    
   const file2 = createReadStream(path.resolve(__dirname, './test2.pdf'));
   await pdfStream.append(file2, { name: 'file2' });
   await pdfStream.finalize();
