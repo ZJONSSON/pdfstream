58aa85fa1a80984b7e691a2b62dc92168260ec31
zjonsson <ziggy.jonsson.nyc@gmail.com>
2023-07-23 21:03:16 -0400

    Test multiple appens

--- test/basic-test.js
+++ test/basic-test.js
@@ -20,3 +20,16 @@ t.test('merges two pdf files', async t => {
   await pdfStream.finalize();
 });
 
+t.test('appending multiple file fails', async t => {
+  const pdfStream = new PdfStream();
+  const file1 = createReadStream(path.resolve(__dirname, './test.pdf'));
+  pdfStream.append(file1,{ name: 'file1' });
+  const file2 = createReadStream(path.resolve(__dirname, './test2.pdf'))
+  pdfStream.append(file2, { name: 'file2' })
+    .then(
+      () => { throw new Error('Non-pdf file should error');},
+      (e) => { t.equal(e.message, 'Previous append has not finished'); }
+    );
+
+})
+
